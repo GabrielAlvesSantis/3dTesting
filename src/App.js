4bb720e6b47f671acc46f781ee7c23d4200ea11e
Gabriel Santis <109669855+GabrielAlvesSantis@users.noreply.github.com>
2022-08-25 10:20:05 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import styled from "styled-components";
+
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
+
+import { Canvas } from "@react-three/fiber";
+
+export default function App() {
+  return (
+    <Wrapper className="App">
+      <Background />
+      <TextSection />
+      <Canvas></Canvas>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+`;
